e8ab6ab56af199d1ee5d4da90bca58f2125ba9c0
Lucas Furlan <furlan@hotmail.com.br>
2023-12-27 03:23:21 -0300

    feat: changed the localstorage to api

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,51 +1,62 @@
-import {FormEvent, useContext} from 'react';
-import {ListItemsContext, ListTasksProps} from '../pages/Index';
-import {CheckOne, Delete, Round} from '@icon-park/react';
-import {Button} from './Button';
+import { FormEvent, useContext } from "react";
+import { ListItemsContext, ListTasksProps } from "../pages/Index";
+import { CheckOne, Delete, Round } from "@icon-park/react";
+import { Button } from "./Button";
+import api from "../services/api";
 
 interface TaskProps {
   task: ListTasksProps;
 }
 
-export function Task({task}: TaskProps) {
-  const {tasks, handleUpdateTasks} = useContext(ListItemsContext);
-  
+export function Task({ task }: TaskProps) {
+  const { tasks, handleUpdateTasks } = useContext(ListItemsContext);
+
   function handleChangeStatus(event: FormEvent) {
-    const idTaskToDelete = event.currentTarget.getAttribute('data-id');
-    
-    const newListTasksWithChangeStatusById: ListTasksProps[] | undefined = tasks?.map(task => {
-      if (task.id === idTaskToDelete) {
-        const newStatusTask = task.status === 1 ? 0 : 1;
-        return {
-          ...task,
-          status: newStatusTask
-        };
-      } else {
-        return {...task};
-      }
-    });
-    
+    const idTaskToChangeStatus = event.currentTarget.getAttribute("data-id");
+
+    const newListTasksWithChangeStatusById: ListTasksProps[] | undefined =
+      tasks?.map((task) => {
+        if (task.id === idTaskToChangeStatus) {
+          const isDone = !task.isDone;
+
+          api.patch(`tasks/${idTaskToChangeStatus}`, { isDone });
+
+          return {
+            ...task,
+            isDone,
+          };
+        } else {
+          return { ...task };
+        }
+      });
+
     handleUpdateTasks(newListTasksWithChangeStatusById);
   }
-  
+
   function handleDeleteTask(e: FormEvent) {
     e.stopPropagation();
-    
+
     const idTaskToDelete = task.id;
-    
-    const newListTaskWithoutTaskToDelete = tasks?.filter(task => {
+
+    const newListTaskWithoutTaskToDelete = tasks?.filter((task) => {
       return task.id !== idTaskToDelete;
     });
-    
+
+    api.delete(`tasks/${idTaskToDelete}`);
+
     handleUpdateTasks(newListTaskWithoutTaskToDelete);
   }
-  
+
   return (
     <li
       key={task.id}
       className={`flex items-baseline gap-3 p-4 mb-4 border rounded-md
         transition-all duration-300 cursor-pointer
-        ${task.status === 0 ? 'bg-gray-500 text-gray-100 border-gray-400 hover:bg-gray-400' : 'bg-gray-500 text-gray-300 border-gray-500 line-through'}
+        ${
+          task.isDone
+            ? "bg-gray-500 text-gray-300 border-gray-500 line-through"
+            : "bg-gray-500 text-gray-100 border-gray-400 hover:bg-gray-400"
+        }
       `}
       data-id={task.id}
       onClick={handleChangeStatus}
@@ -54,27 +65,24 @@ export function Task({task}: TaskProps) {
         className="w-4 h-4 stroke-blue-600 text-lg
             focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2"
       >
-        {task.status === 0
-          ? <Round theme="outline" fill="#8284FA"/>
-          : <CheckOne theme="filled" fill="#5E60CE"/>
-        }
+        {task.isDone ? (
+          <CheckOne theme="filled" fill="#5E60CE" />
+        ) : (
+          <Round theme="outline" fill="#8284FA" />
+        )}
       </span>
-      
-      <span className="w-full">{task.item}</span>
-      
+
+      <span className="w-full">{task.title}</span>
+
       <Button
         className="text-gray-300 p-2 -mr-2 rounded-sm cursor-pointer transition-all duration-300
           hover:text-red
           focus:outline-none focus:border-red focus:ring-2 focus:ring-red"
-        aria-label={`Excluir ${task.item}`}
+        aria-label={`Excluir ${task.title}`}
         onClick={handleDeleteTask}
       >
-        <Delete
-          theme="outline"
-          size={12}
-        />
+        <Delete theme="outline" size={12} />
       </Button>
-    
     </li>
   );
 }
